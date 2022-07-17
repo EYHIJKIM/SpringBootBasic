ea15710ced9eec9dadc9bc3e89456e30ae5714bb
kimze <crk33333@gmail.com>
2022-07-17 17:10:40 +0900

    새로운 할인 정책 개발(rateDiscount)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -10,7 +9,18 @@ public class OrderServiceImpl implements  OrderService{
 
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    /**
+     *
+    OrderServiceImpl이 추상(DiscountPolicy)에만 의존해야 하는데,
+     추상뿐만 아니라 구체(FixDiscountPolicy)에도 의존하고 있음
+        >> DIP 위반
+     -해결
+     1. 구체클레스를 변경할 때 클라이언트 코드(OrderServiceImpl)도 함께 변경해야 한다. 추상에만 의존하도록 변경!
+
+     **/
+    private DiscountPolicy discountPolicy;
+
 
 
     @Override
